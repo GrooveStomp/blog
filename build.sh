0b6e7ba74c86b8d0a27c5b1a92aa69e967b07e3c
Aaron Oman <aaronoman@protonmail.com>
2016-10-29 22:12:13 -0700

    Custom build script with support for legacy feeds.

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+hugo -t groovestomp
+
+# Copy RSS feeds to legacy locations.
+cp public/index.xml public/feed.xml
+cp public/tags/boardgaming/index.xml public/boardgaming_feed.xml
+cp public/tags/programming/index.xml public/programming_feed.xml
