8826f32897d95d48051a9167cb49aa3449306e07
Aaron Oman <aaron@protonmail.com>
2016-10-31 05:05:47 +0000

    post for 2016-10-30 and update build script.

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 hugo -t groovestomp
 
@@ -6,4 +6,9 @@ hugo -t groovestomp
 cp public/index.xml public/feed.xml
 cp public/tags/boardgaming/index.xml public/boardgaming_feed.xml
 cp public/tags/programming/index.xml public/programming_feed.xml
-sudo cp -r public/* /var/www/blog/
+
+if [[ "$1" == "release" ]]; then
+    sudo cp -r public/* /var/www/blog/
+elif [[ "$1" == "test" ]]; then
+    sudo cp -r public/* /var/www/blog-test/
+fi
